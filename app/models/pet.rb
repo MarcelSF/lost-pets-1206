56b8365bf1b3b625fc1ddc9021ca638efc3ef42b
Marcel Fonseca <marcelsf23br@gmail.com>
2023-05-23 18:07:14 +0200

    CRUD done. Add Simple form

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,11 @@
 class Pet < ApplicationRecord
+  SPECIES = %w[ant dog cat snake raccoon hamster turtle parrot]
+
   validates :name, presence: true
   # Make sure that our pets are inside a specific list of species
-  validates :species, inclusion: { in:  %w[ant dog cat snake raccoon hamster turtle parrot] }
+  validates :species, inclusion: { in: SPECIES }
+
+  def found_days_ago
+    "Found #{(Date.today - found_on).to_i} days ago"
+  end
 end
